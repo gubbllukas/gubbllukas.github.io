4ef3cdb068cd3a25611cbcc7347549c3be7d48ad
gubbllukas <l.endres90@googlemail.com>
2020-03-31 19:11:10 +0200

    Notizen

--- nz/main.js
+++ nz/main.js
@@ -9,11 +9,13 @@ L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
 
+//Marker für River Safari und weiteres PopUp dazu:
 var markerRiverSafari = L.marker([-43.853582, 169.054602]).addTo(mymap);
 markerRiverSafari.bindPopup("<b>Haast River Safari!</b><br>Experience real New Zealand.").openPopup();
 
+//Marker für Stadt mit PopUp:
 var markerStadt = L.marker([-43.880833, 169.040278]).addTo(mymap);
-markerStadt.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+markerStadt.bindPopup("<b>Haast!</b><br>a cosy and quite town to stay.").openPopup();
 
 // Beim Klicken auf beliebigen Punkt auf der Karte werden entsprechende Koordinaten angezeigt:
 var popup = L.popup();
