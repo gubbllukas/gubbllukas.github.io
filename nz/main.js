53af2669dc4545dd150c58deac4b57231e6317e9
gubbllukas <l.endres90@googlemail.com>
2020-03-31 18:22:06 +0200

    Karte anzeigen

--- nz/main.js
+++ nz/main.js
@@ -1,2 +1,10 @@
 var mymap = L.map('map').setView([-43.880833, 169.040278], 13);
 
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 17,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
