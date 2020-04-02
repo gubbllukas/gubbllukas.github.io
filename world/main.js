64f4893a8febfcbbbb79470519c97ee099e6e051
gubbllukas <l.endres90@googlemail.com>
2020-04-02 18:03:15 +0200

    Einbau Funktion

--- world/main.js
+++ world/main.js
@@ -1,6 +1,6 @@
 let startLayer = L.tileLayer.provider("OpenTopoMap")
 let map = L.map("map", {
-    center: [0,0],
+    center: [0, 0],
     zoom: 2,
     layers: [
         startLayer
@@ -16,33 +16,35 @@ L.control.layers({
     "Esri.OceanBasemap": L.tileLayer.provider("Esri.OceanBasemap"),
     "NASAGIBS.ViirsEarthAtNight2012": L.tileLayer.provider("NASAGIBS.ViirsEarthAtNight2012"),
     "Stamen.Watercolor": L.tileLayer.provider("Stamen.Watercolor")
-},{
-    "Thematische Darstellung" : circleGroup
+}, {
+    "Thematische Darstellung": circleGroup
 }).addTo(map);
-
-
 
 // L.marker([0,0]).addTo(map);
 // console.log(CONFIRMED);
 // for-Schleife über alle Arrays der CONFIRMED Einträge:
 
-for (let i = 1; i < CONFIRMED.length; i++) {
-    let row = CONFIRMED[i];
-    // console.log(row[2],row[3]);
-    let reg = `${row[0]} ${row[1]}`;
-    let lat = row[2];
-    let lng = row[3];
-    let val = row[row.length-1];
-    // let mrk = L.marker([lat,lng]).addTo(map);
-    // mrk.bindPopup(`${reg}: ${val}`);
+let drawCircles = function (data) {
+    for (let i = 1; i < data.length; i++) {
+        let row = data[i];
+        // console.log(row[2],row[3]);
+        let reg = `${row[0]} ${row[1]}`;
+        let lat = row[2];
+        let lng = row[3];
+        let val = row[row.length - 1];
+        // let mrk = L.marker([lat,lng]).addTo(map);
+        // mrk.bindPopup(`${reg}: ${val}`);
 
-    // A = r²*PI
-    // r² = A/PI
-    // r = WURZEL(A/PI)
-    let s = 0.2
-    let r = Math.sqrt(val*s/Math.PI);
-    let circle = L.circleMarker([lat,lng],{
-        radius: r
-    }).addTo(circleGroup);
-    circle.bindPopup(`${reg}: ${val}`);
-}
+        // A = r²*PI
+        // r² = A/PI
+        // r = WURZEL(A/PI)
+        let s = 0.2
+        let r = Math.sqrt(val * s / Math.PI);
+        let circle = L.circleMarker([lat, lng], {
+            radius: r
+        }).addTo(circleGroup);
+        circle.bindPopup(`${reg}: ${val}`);
+    }
+};
+
+drawCircles(CONFIRMED);
