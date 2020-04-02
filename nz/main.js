5f0c4b8f87f520de6deb95867089913b0e292103
gubbllukas <l.endres90@googlemail.com>
2020-04-02 15:51:43 +0200

    Main.js Anpassungen

--- nz/main.js
+++ nz/main.js
@@ -1,4 +1,4 @@
-var mymap = L.map('map').setView([-43.880833, 169.040278], 13);
+let mymap = L.map('map').setView([-43.880833, 169.040278], 13);
 
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
@@ -10,15 +10,16 @@ L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 }).addTo(mymap);
 
 //Marker für River Safari und weiteres PopUp dazu:
-var markerRiverSafari = L.marker([-43.853582, 169.054602]).addTo(mymap);
+let markerRiverSafari = L.marker([-43.853582, 169.054602]).addTo(mymap);
 markerRiverSafari.bindPopup("<b>Haast River Safari!</b><br>Experience real New Zealand.").openPopup();
 
 //Marker für Stadt mit PopUp:
-var markerStadt = L.marker([-43.880833, 169.040278]).addTo(mymap);
+let markerStadt = L.marker([-43.880833, 169.040278]).addTo(mymap);
 markerStadt.bindPopup("<b>Haast!</b><br>a cosy and quite town to stay.").openPopup();
 
 // Beim Klicken auf beliebigen Punkt auf der Karte werden entsprechende Koordinaten angezeigt:
-var popup = L.popup();
+let popup = L.popup();
+
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
@@ -26,6 +27,3 @@ function onMapClick(e) {
         .openOn(mymap);
 }
 mymap.on('click', onMapClick);
-
-
-
