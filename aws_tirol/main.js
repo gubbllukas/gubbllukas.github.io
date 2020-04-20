db02d8c6de7ca75742322642be116ed35bd57a48
gubbllukas <l.endres90@googlemail.com>
2020-04-20 20:36:07 +0200

    Stationen anzeigen, Filter wenn Temp, Liste vollständig, Grafik als Link

--- aws_tirol/main.js
+++ aws_tirol/main.js
@@ -8,6 +8,8 @@ let map = L.map("map", {
     ]
 });
 
+let awsLayer = L.featureGroup().addTo(map);
+
 L.control.layers({
     "BasemapAT.grau": startLayer,
     "BasemapAT": L.tileLayer.provider("BasemapAT"),
@@ -20,6 +22,32 @@ L.control.layers({
         L.tileLayer.provider("BasemapAT.orthofoto"),
         L.tileLayer.provider("BasemapAT.overlay")
     ])
+}, {
+    "Wetterstationen Tirol": awsLayer
 }).addTo(map);
 
 let awsUrl = "https://aws.openweb.cc/stations";
+
+let aws = L.geoJson.ajax(awsUrl, {
+    filter: function (feature) {
+        //console.log("Feature in filter: ", feature) ---> so abändern, dass nur wenn LT vorhanden, dann anzeigen;
+        return feature.geometry.LT = true;
+    },
+    pointToLayer: function (point, latlng) {
+        // console.log("point: ", point);
+        // Liste Schneehöhe fehlt noch, da Wert in properties nicht gefunden?!
+        // <li>Schneehöhe: ${point.properties.???} [Einheit?]]</li>
+        let marker = L.marker(latlng).bindPopup(`
+        <h3>${point.properties.name}, ${point.geometry.coordinates[2]} m</h3>
+        <ul>
+        <li>Position: Lat: ${point.geometry.coordinates[1]}/Lng: ${point.geometry.coordinates[0]}</li>
+        <li>Datum: ${point.properties.date}</li>
+        <li>Lufttemperatur: ${point.properties.LT} °C</li>
+        <li>Windgeschwindigkeit: ${point.properties.WG} m/s</li>
+        <li>Relative Feuchte: ${point.properties.RH} %</li>
+        <li><a target="links" href="https://lawine.tirol.gv.at/data/grafiken/1100/standard/tag/${point.properties.plot}.png">ZAMG Wetterdaten Grafik</a></li>
+        </ul>
+        `);
+        return marker;
+    }
+}).addTo(awsLayer);
