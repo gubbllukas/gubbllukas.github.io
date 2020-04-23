4bf318074fab3163d9d3322fa6553f6e6d10230f
gubbllukas <l.endres90@googlemail.com>
2020-04-23 15:46:47 +0200

    AN- und Ausschaltbares Overlay Objekt

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -9,6 +9,9 @@ let map = L.map("map", {
 });
 
 let awsLayer = L.featureGroup().addTo(map);
+let overlay = {
+    stations: L.featureGroup()
+}
 
 L.control.layers({
     "BasemapAT.grau": startLayer,
@@ -23,7 +26,7 @@ L.control.layers({
         L.tileLayer.provider("BasemapAT.overlay")
     ])
 }, {
-    "Wetterstationen Tirol": awsLayer
+    "Wetterstationen Tirol": overlay.stations
 }).addTo(map);
 
 let awsUrl = "https://aws.openweb.cc/stations";
@@ -48,4 +51,4 @@ let aws = L.geoJson.ajax(awsUrl, {
         `);
         return marker;
     }
-}).addTo(awsLayer);
+}).addTo(overlay.stations);
